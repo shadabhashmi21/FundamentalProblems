2ada1fceda181ec905e057acd0c0fffef9e95094
shadabhashmi21 <er.shadabhashmi@gmail.com>
2021-08-25 06:14:16 +0100

    feat: Palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Program to check if a String is Palindrome or not.
+ * @author shadab
+ */
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("PALINDROME");
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String str = ob.nextLine();
+        System.out.println(isPalindrome(str) ? "The number is palindrome" : "The number is not palindrome");
+    }
+    
+    static boolean isPalindrome(String word){
+        char[] ar = word.toCharArray();
+        StringBuilder sb = new StringBuilder(word.length());
+        
+        for(int i = word.length() - 1 ; i >= 0 ; i--) {
+            sb.append(ar[i]);
+        }
+        
+        return word.equals(sb.toString());
+    }
+}
---
+++ src/PalindromeUsingReverse.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/**
+ * Program to check if a String is Palindrome or not using reverse method.
+ * @author Shadab 
+ */
+public class PalindromeUsingReverse {
+    public static void main(String[] args) {
+        System.out.println("PALINDROME USING REVERSE");
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String str = ob.nextLine();
+        System.out.println(isPalindrome(str) ? "The number is palindrome" : "The number is not palindrome");
+    }
+    
+    static boolean isPalindrome(String word) {
+        StringBuffer sb = new StringBuffer(word);
+        return word.equals(sb.reverse().toString());
+    }
+}
